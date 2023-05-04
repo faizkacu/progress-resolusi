0a2a4fe81ab61145c1abdccb459626a5d8511f09
faizkacu <112756921+faizkacu@users.noreply.github.com>
2023-05-04 12:36:10 +0700

    Add files via upload

---
+++ kami.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>                         
+<body>
+    <form action="kami.php" method="post">
+        Nama :<input type="test" name="nama"><br>
+        Umur :<input type="test" name="umur"><br>
+        <button type="submit" >Kirim</button>
+    </form>
+</body>
+</html>
+
+<h1>Nama Saya <?php echo $_POST['nama'] ?> Umur Saya <?php echo $_POST['umur'] ?> </h1>
+
+
+<?php
+
+//if(isset($_POST['kirim'])) {
+
+//var_dump($_POST);
+//echo "Nama Saya" . $_POST['nama'];
+//echo "<br>";
+//echo "Umur Saya" . $_POST['umur'];
+//} else {
+  //  echo "Krim data terlebih dahulu";
+//}
+
+
+
+//echo "<h1>" . "Nama Saya " . $_POST['nama'] . "," . "dan umur saya " . $_POST['umur'];  
+
+
+
+
+
+
+
+//
+?>
